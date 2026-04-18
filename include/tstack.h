b4555f2c874dee256d11e4f4637f3e8faf27872e
whoany <120975118+whoany@users.noreply.github.com>
2026-04-18 15:06:27 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,37 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T data[N];
+    int top;
+
+ public:
+    TStack() : top(-1) {}
+
+    bool isEmpty() const {
+        return top == -1;
+    }
+
+    bool isFull() const {
+        return top == N - 1;
+    }
+
+    void push(T value) {
+        if (!isFull()) {
+            top++;
+            data[top] = value;
+        }
+    }
+
+    T pop() {
+        T value = data[top];
+        top--;
+        return value;
+    }
+
+    T peek() const {
+        return data[top];
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
